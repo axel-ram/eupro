3c8f5e03b24e23867969a3b44b0fd63686dc4434
Ramnath Sharma <axel-ram@users.noreply.github.com>
2019-04-14 02:21:21 +0530

    Rename problem 21.py to problem 22.py

---
+++ problem 22.py
@@ -0,0 +1,27 @@
+import math
+n=220
+majorsum=0
+for n in range(2,10001):
+    k=2
+    tempsum=1
+    while(k*k<=n):
+        if n%k==0:
+            if k!=n/k:
+                tempsum+=k+int(n/k)
+            else:
+                tempsum+=k
+        k+=1
+    k=2
+    divSum=tempsum
+    tempsum=1
+    while(k*k<=divSum):
+        if divSum%k==0:
+            if k!=divSum/k:
+                tempsum+=k+int(divSum/k)
+            else:
+                tempsum+=k
+        k+=1
+    if n==tempsum and n!=divSum:
+        majorsum+=n+divSum
+        #print(n,divSum)
+print(int(majorsum/2))
